753c9de0404549c0d75c9f2835ee4c674cc97197
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-06-04 22:30:47 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export class DigitalClicker extends Component {
+   state = {
+    timesClicked: 0 
+   }
+
+   simulate = () => {
+       let newNum = this.state.timesClicked + 1 
+       this.setState({timesClicked: newNum})
+   }
+    render() {
+        return (
+            <div>
+                <button onClick={this.simulate}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
+
+export default DigitalClicker
+
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,45 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export class YouTubeDebugger extends Component {
+    state = {
+        errors: [],
+        user: null, 
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+
+    changeBirate = () => {
+       if (this.state.settings.bitrate === 8){
+       this.setState({settings: {...this.state.settings, bitrate: 12} })
+       } else {
+        this.setState({settings: {...this.state.settings, bitrate: 8} })
+       }
+    }
+
+    changeResolution = () => {
+        if (this.state.settings.video.resolution === '1080p'){
+        this.setState({settings: {...this.state.settings, video: {resolution: '720p'} }})
+        } else {
+            this.setState({settings: {...this.state.settings, video: {resolution: '1080p'} }}) 
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>birate: {this.state.settings.bitrate}</p>
+                <button onClick={this.changeBirate} className='bitrate'>change birate</button>
+                <p>resolution: {this.state.settings.video.resolution}</p>
+                <button onClick={this.changeResolution} className='resolution'>Change resolution</button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
+
